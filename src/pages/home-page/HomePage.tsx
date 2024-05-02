6f9823786a46ce4862ed6125846f5cf593649999
xalivo <87896066+xalivo@users.noreply.github.com>
2024-05-02 11:53:29 +0200

    added changes to home page responsive etc

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -12,66 +12,89 @@ import {
     IonCardContent,
     IonCardHeader,
     IonCardSubtitle,
-    IonCardTitle, IonContent,
-    IonHeader
+    IonCardTitle, IonCol, IonContent, IonGrid,
+    IonHeader, IonPage, IonRow
 } from "@ionic/react";
 
 const HomePage = () => {
     return (
         <>
-            <NavBar/>
-            <div className={"flex flex-col gap-10"}>
-                <div className={"text-5xl mt-10 text-center font-bold"}>Araceli - a simple file management & cloud platform</div>
-                <div className={"flex flex-row justify-evenly flex-wrap"}>
-                    <IonCard style={{width: 450}}>
-                        <IonCardHeader>
-                            <IonCardTitle>Sign up today & get 0.0001 GB free storage space!</IonCardTitle>
-                            <IonCardSubtitle>Sign up with Google or create an Araceli account</IonCardSubtitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            <IonButton>Sign up</IonButton>
-                        </IonCardContent>
-                    </IonCard>
-                    <IonCard style={{width: 450}}>
-                        <IonCardHeader>
-                            <IonCardTitle>Already have an account? Log into your account.</IonCardTitle>
-                            <IonCardSubtitle>Login with Google, or with your username/email and password</IonCardSubtitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            <IonButton>Login</IonButton>
-                        </IonCardContent>
-                    </IonCard>
-                </div>
-                <div className={"flex flex-row justify-evenly"}>
-                    <IonCard>
-                        <img alt="Silhouette of mountains" height={"100"} src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                        <IonCardHeader>
-                            <IonCardTitle>Simple & Fast.</IonCardTitle>
-                        </IonCardHeader>
+            <IonPage>
+                <IonContent>
+                    <NavBar/>
+                    <div className={"flex flex-col gap-10"}>
+                        <div className={"text-5xl mt-10 text-center font-bold"}>Araceli - a simple file management &
+                            cloud platform
+                        </div>
+                        <div className={"flex flex-row justify-evenly flex-wrap"}>
+                            <IonCard style={{width: 450}}>
+                                <IonCardHeader>
+                                    <IonCardTitle>Sign up today & get 0.0001 GB free storage space!</IonCardTitle>
+                                    <IonCardSubtitle>Sign up with Google or create an Araceli account</IonCardSubtitle>
+                                </IonCardHeader>
+                                <IonCardContent>
+                                    <IonButton>Sign up</IonButton>
+                                </IonCardContent>
+                            </IonCard>
+                            <IonCard style={{width: 450}}>
+                                <IonCardHeader>
+                                    <IonCardTitle>Already have an account? Log into your account.</IonCardTitle>
+                                    <IonCardSubtitle>Login with Google, or with your username/email and
+                                        password</IonCardSubtitle>
+                                </IonCardHeader>
+                                <IonCardContent>
+                                    <IonButton>Login</IonButton>
+                                </IonCardContent>
+                            </IonCard>
+                        </div>
+                        <div className={"flex flex-row justify-evenly"}>
+                            <IonGrid>
+                                <IonRow>
+                                    <IonCol sizeLg={"4"}>
+                                        <IonCard>
+                                            <img alt="Silhouette of mountains" height={"100"}
+                                                 src="https://ionicframework.com/docs/img/demos/card-media.png"/>
+                                            <IonCardHeader>
+                                                <IonCardTitle>Simple & Fast.</IonCardTitle>
+                                            </IonCardHeader>
 
-                        <IonCardContent>Araceli tries to be as simple as possible in order to provide the optimal user experience. We use modern and fast frameworks to ensure fast loading times.</IonCardContent>
-                    </IonCard>
-                    <IonCard>
-                        <img alt="Silhouette of mountains" height={"100"} src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                        <IonCardHeader>
-                            <IonCardTitle>Open-source.</IonCardTitle>
-                        </IonCardHeader>
+                                            <IonCardContent>Araceli tries to be as simple as possible in order to provide the
+                                                optimal user experience. We use modern and fast frameworks to ensure fast loading
+                                                times.</IonCardContent>
+                                        </IonCard>
+                                    </IonCol>
+                                    <IonCol sizeXs={"12"} sizeLg={"4"}>
+                                        <IonCard>
+                                            <img alt="Silhouette of mountains" height={"100"}
+                                                 src="https://ionicframework.com/docs/img/demos/card-media.png"/>
+                                            <IonCardHeader>
+                                                <IonCardTitle>Open-source.</IonCardTitle>
+                                            </IonCardHeader>
 
-                        <IonCardContent></IonCardContent>
-                    </IonCard>
-                    <IonCard>
-                        <img alt="Silhouette of mountains" height={"100"} src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                        <IonCardHeader>
-                            <IonCardTitle>Documentation</IonCardTitle>
-                        </IonCardHeader>
+                                            <IonCardContent></IonCardContent>
+                                        </IonCard>
+                                    </IonCol>
+                                    <IonCol sizeLg={"4"}>
+                                        <IonCard>
+                                            <img alt="Silhouette of mountains" height={"100"}
+                                                 src="https://ionicframework.com/docs/img/demos/card-media.png"/>
+                                            <IonCardHeader>
+                                                <IonCardTitle>Documentation</IonCardTitle>
+                                            </IonCardHeader>
 
-                        <IonCardContent className={"flex flex-col gap-3"}>
-                            This application is well-documented
-                            <IonButton>Go to documentation</IonButton>
-                        </IonCardContent>
-                    </IonCard>
-                </div>
-            </div>
+                                            <IonCardContent className={"flex flex-col gap-3"}>
+                                                This application is well-documented.
+                                                <IonButton>Go To Documentation</IonButton>
+                                            </IonCardContent>
+                                        </IonCard>
+                                    </IonCol>
+                                </IonRow>
+                            </IonGrid>
+                        </div>
+                    </div>
+                </IonContent>
+            </IonPage>
+
 
         </>
     );
